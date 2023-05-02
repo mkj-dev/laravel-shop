c450d84fc388f9ffa6f45d5a49da1f8a98b204f2
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-05-02 19:15:42 +0200

    #4 Dodanie dodatkowych pól do modelu użytkownika - pierwsza migracja

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,6 +9,7 @@
                     <th scope="col">Email</th>
                     <th scope="col">Imię</th>
                     <th scope="col">Nazwisko</th>
+                    <th scope="col">Numer telefonu</th>
                     <th scope="col">Akcje</th>
                 </tr>
             </thead>
@@ -18,8 +19,9 @@
                         <th scope="row">{{ $user->id }}</th>
                         <td>{{ $user->email }}</td>
                         <td>{{ $user->name }}</td>
+                        <td>{{ $user->surname }}</td>
+                        <td>{{ $user->phone_number }}</td>
                         <td>-</td>
-                        <td></td>
                     </tr>
                 @endforeach
             </tbody>
