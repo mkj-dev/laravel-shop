32fd02bacf15f51ca6db41034820175d469ceda6
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-05-05 18:57:58 +0200

    Dodano nowe pola do schematu tabeli

--- database/migrations/2023_05_05_113711_create_products_table.php
+++ database/migrations/2023_05_05_113711_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 500);
+            $table->string('description', 1500);
+            $table->integer('amount');
+            $table->decimal('price');
             $table->timestamps();
         });
     }
