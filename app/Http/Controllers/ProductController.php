faa410e08f7e3b18e04f4367684f2ca978f7bdec
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-05-06 15:56:46 +0200

    #3 Lista produktów dla niezalogowanego użytkownika - Dodano możliwość zapisu i aktualizacji ścieżki do pliku ze zdjęciem produktu

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -40,7 +40,9 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         $product = new Product($request->all());
-        $product->image_path = $request->file('image')->store('products');
+        if ($request->hasFile('image')) {
+            $product->image_path = $request->file('image')->store('products');
+        }
         $product->save();
         return redirect(route('products.index'));
     }
@@ -82,6 +84,9 @@ class ProductController extends Controller
     public function update(Request $request, Product $product)
     {
         $product->fill($request->all());
+        if ($request->hasFile('image')) {
+            $product->image_path = $request->file('image')->store('products');
+        }
         $product->save();
         return redirect(route('products.index'));
     }
