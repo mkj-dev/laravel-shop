ac219c4167cb781e0055ec10fa60fd9efc94f0df
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-05-11 13:13:32 +0200

    #8 Dodanie obsługi języka polskiego do naszego sklepu - dodano nowy plik z tłumaczeniami formularza dodawania i edycji produktu

---
+++ resources/lang/pl/shop.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+  'product' => [
+    'product_form' => [
+      'title' => 'Dodawanie produktu',
+      'name' => 'Nazwa produktu',
+      'description' => 'Opis produktu',
+      'amount' => 'Ilość',
+      'price' => 'Cena',
+      'image' => 'Zdjęcie produktu',
+    ],
+    'edit_form' => [
+      'title' => 'Edycja produktu',
+    ]
+  ],
+  'button' => [
+    'save' => 'Zapisz',
+  ]
+];
