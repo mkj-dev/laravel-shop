cba7dd72085178c34159959e2f32d425dbcbfcf4
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-05-11 13:14:24 +0200

    #8 Dodanie obsługi języka polskiego do naszego sklepu - dodano dynamiczne tłumaczenie do formularza

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -5,14 +5,14 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Dodawanie produktu</div>
+                    <div class="card-header">{{ __('shop.product.product_form.title') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('products.store') }}" enctype="multipart/form-data">
                             @csrf
 
                             <div class="row mb-3">
-                                <label for="name" class="col-md-4 col-form-label text-md-end">Nazwa</label>
+                                <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('shop.product.product_form.name') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="name" type="text" maxlength="500"
@@ -28,7 +28,7 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="description" class="col-md-4 col-form-label text-md-end">Opis</label>
+                                <label for="description" class="col-md-4 col-form-label text-md-end">{{ __('shop.product.product_form.description') }}</label>
 
                                 <div class="col-md-6">
                                     <textarea id="description" minlength="10" maxlength="1500" class="form-control @error('description') is-invalid @enderror"
@@ -42,7 +42,7 @@
 							</div>
 
 							<div class="row mb-3">
-								<label for="amount" class="col-md-4 col-form-label text-md-end">Ilość</label>
+								<label for="amount" class="col-md-4 col-form-label text-md-end">{{ __('shop.product.product_form.amount') }}</label>
 
 								<div class="col-md-6">
 									<input id="amount" type="number" min="0"
@@ -59,7 +59,7 @@
 
 
 							<div class="row mb-3">
-								<label for="price" class="col-md-4 col-form-label text-md-end">Cena</label>
+								<label for="price" class="col-md-4 col-form-label text-md-end">{{ __('shop.product.product_form.price') }}</label>
 
 								<div class="col-md-6">
 									<input id="price" type="number" step="0.01" min="0"
@@ -75,7 +75,7 @@
 							</div>
 
 							<div class="row mb-3">
-								<label for="image" class="col-md-4 col-form-label text-md-end">Zdjęcie produktu</label>
+								<label for="image" class="col-md-4 col-form-label text-md-end">{{ __('shop.product.product_form.image') }}</label>
 
 								<div class="col-md-6">
 									<input id="image" type="file" class="form-control @error('image') is-invalid @enderror" name="image">
@@ -91,7 +91,7 @@
 							<div class="row mb-0">
 								<div class="col-md-6 offset-md-4">
 									<button type="submit" class="btn btn-primary">
-										Zapisz
+										{{ __('shop.button.save') }}
 									</button>
 								</div>
 							</div>
